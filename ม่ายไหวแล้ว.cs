eda62eadff441649cee88298e6116dc0f59d68bd
atichart4235 <chayanon.bunprasert@mail.kmutt.ac.th>
2021-03-28 18:06:27 +0700

    ข้อ2

---
+++ ม่ายไหวแล้ว.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("please input a:");
+            a = int.Parse(Console.ReadLine());
+            int b;
+            Console.Write("please input b:");
+            b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+            while(x!=y)
+            {
+                if (x<y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
